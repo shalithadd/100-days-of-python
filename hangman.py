3178bfaed515363ea3fae0bd13ef0834fa61d738
Shalitha Dehigaspitiyage <shalitha.dehigaspitiyage@gmail.com>
2023-03-19 21:27:18 +0000

    change logic as shown in the tutorial

--- hangman.py
+++ hangman.py
@@ -1,19 +1,23 @@
 import random
 
 
-words = ["aardvark", "baboon", "camel"]
+words = ["aardvark", "baboon", "camel", "apple", "pizza"]
 random_word = random.choice(words)
 guessed_letters = []
+word_length = len(random_word)
 
-for char in range(len(random_word)):
+# Using "_" in the for loop as value of variable is not needed
+for _ in range(word_length):
     guessed_letters.append("_")
 
 print(" ".join(guessed_letters))
 lives = 6
 while lives > 0:
     user_guess = input("Guess a letter: ").lower()
-    for idx, letter in enumerate(random_word):
+
+    for position in range(word_length):
+        letter = random_word[position]
         if letter == user_guess:
-            guessed_letters[idx] = user_guess
+            guessed_letters[position] = letter
 
     print(" ".join(guessed_letters))
