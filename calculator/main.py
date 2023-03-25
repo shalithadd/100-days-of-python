267176421c66abe14ce596c9a843c628ec1d0f68
Shalitha Dehigaspitiyage <shalitha.dehigaspitiyage@gmail.com>
2023-03-25 11:31:06 +0000

    all complete and tested

--- calculator/main.py
+++ calculator/main.py
@@ -1,4 +1,9 @@
 from art import logo
+import os
+
+
+def clear():
+    os.system("cls")
 
 
 def add(n1, n2):
@@ -24,25 +29,33 @@ operations = {
     "/": divide,
 }
 
-print(logo)
-should_continue = True
-new_calculation = True
-num1 = int(input("What's the first number?: "))
-for symbol in operations:
-    print(symbol)
 
-while should_continue:
-    operator = input("Select the operation: ")
-    num2 = int(input("What's the next number?: "))
-    calculation_function = operations[operator]
-    answer = calculation_function(n1=num1, n2=num2)
+def calculator():
+    clear()
+    print(logo)
 
-    print(f"{num1} {operator} {num2} = {answer}")
+    should_continue = True
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
 
-    if (
-        input(f"Type 'y' to continue calculating with {answer} or type 'q' to exit: ")
-        == "y"
-    ):
-        num1 = answer
-    else:
-        should_continue = False
+    while should_continue:
+        operator = input("Select the operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operator]
+        answer = calculation_function(n1=num1, n2=num2)
+
+        print(f"{num1} {operator} {num2} = {answer}")
+        user_choice = input(
+            f"Type 'y' to continue calculating with {answer} or type 'n' to start new calculation or type 'q' to exit: "
+        )
+
+        if user_choice == "y":
+            num1 = answer
+        elif user_choice == "n":
+            calculator()
+        else:
+            should_continue = False
+
+
+calculator()
