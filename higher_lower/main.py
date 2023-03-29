0818ec5be0c6141f3db27a3c08761eed58866458
Shalitha Dehigaspitiyage <shalitha.dehigaspitiyage@gmail.com>
2023-03-29 19:41:38 +0100

    all working

--- higher_lower/main.py
+++ higher_lower/main.py
@@ -1,6 +1,11 @@
 import random
 from art import logo, vs
 from game_data import data
+import os
+
+
+def clear():
+    os.system("cls")
 
 
 def set_compare():
@@ -20,22 +25,40 @@ def compare_followers(follower_count_a, follower_cont_b, answer, score):
         winner = "b"
 
     if winner == answer:
-        print(f"You are right! Current score: {score + 1}.")
-        return score + 1
+        message = f"You are right! Current score: {score + 1}."
+        return score + 1, winner, message
     else:
-        print(f"Sorry that's wrong. Final score: {score}.")
+        message = f"Sorry that's wrong. Final score: {score}."
+        return score, winner, message
 
 
 def game():
+    clear()
     print(logo)
-    bio_a, follower_count_a = set_compare()
-    bio_b, follower_count_b = set_compare()
+    correct_answer = True
     score = 0
-    print(f"Compare A: {bio_a}.")
-    print(vs)
-    print(f"Compare B: {bio_b}.")
-    answer = input("Who has more followers 'A' or 'B': ").lower()
-    score = compare_followers(follower_count_a, follower_count_b, answer, score)
+    bio_a, follower_count_a = set_compare()
+
+    while correct_answer:
+        bio_b, follower_count_b = set_compare()
+        print(f"Compare A: {bio_a}.")
+        print(vs)
+        print(f"Compare B: {bio_b}.")
+        answer = input("Who has more followers 'A' or 'B': ").lower()
+        score, winner, message = compare_followers(
+            follower_count_a, follower_count_b, answer, score
+        )
+        if winner == answer:
+            clear()
+            print(logo)
+            print(message)
+            bio_a = bio_b
+            follower_count_a = follower_count_b
+        else:
+            correct_answer = False
+            clear()
+            print(logo)
+            print(message)
 
 
 game()
