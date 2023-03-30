5ea1c99cbb4d1fba8413add645dc7e99ac168729
Shalitha Dehigaspitiyage <shalitha.dehigaspitiyage@gmail.com>
2023-03-30 22:54:47 +0100

    all working fine

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -1,3 +1,5 @@
+
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -31,25 +33,84 @@ resources = {
     "money": 0,
 }
 
-user_prompt = input("What would you like? (espresso/latte/cappuccino): ")
 
-# TODO: 1. Print report - print all the resources in the machine including any money when user prompt "report"
-if user_prompt == "report":
+def print_report():
     for key, value in resources.items():
-        print(f"{key}: {value}")
-
-# TODO: 2. If user prompt is 'off' exit the program
-
-
-# TODO: 3. Check if resources are sufficient to make the drink
+        if key == "money":
+            print(f"{key}: ${value}")
+        else:
+            print(f"{key}: {value}")
 
 
-# TODO: 4. Process coins. quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
+def check_resources(order):
+    order_ingredients = MENU[order]["ingredients"]
+    if order_ingredients["water"] > resources["water"]:
+        print("Sorry there's not enough water.")
+    elif order_ingredients["coffee"] > resources["coffee"]:
+        print("Sorry there's not enough coffee.")
+    elif "milk" in order_ingredients.keys() and order_ingredients["milk"] > resources["milk"]:
+        print("Sorry there's not enough milk.")
+    else:
+        return MENU[order]["cost"], order_ingredients
 
 
-# TODO: 5. Check transaction successful, if enough coin make drink and add to profit else refund coins
+def process_coins():
+    coin_types = {
+        "quarters": {
+            "count": 0,
+            "value": .25,
+        },
+        "dimes": {
+            "count": 0,
+            "value": .10,
+        },
+        "nickles": {
+            "count": 0,
+            "value": .05,
+        },
+        "pennies": {
+            "count": 0,
+            "value": .01
+        },
+    }
+    for coin in coin_types:
+        coin_types[coin]["count"] = int(input(f"How many {coin}?: "))
+    # Calculate coins total
+    coins_total = 0
+    for coin in coin_types:
+        coins_total += coin_types[coin]["count"] * coin_types[coin]["value"]
+    return coins_total
 
 
-# TODO: 6. Make coffee, ff transaction successful make coffee and deduct ingredients form resources
+def make_coffee(order, order_ingredients):
+    for ingredient in order_ingredients:
+        resources[ingredient] -= order_ingredients[ingredient]
+    print(f"Here is your {order} ☕. Enjoy!")
+
+
+is_on = True
+while is_on:
+    user_choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if user_choice == "report":
+        print_report()
+        continue
+    elif user_choice == "off":
+        is_on = False
+        break
+    else:
+        price, ingredients = check_resources(user_choice)
+
+    total_coin_value = process_coins()
+    if price > total_coin_value:
+        print("Sorry that's not enough money. Money refunded")
+    elif price == total_coin_value:
+        resources["money"] += price
+        make_coffee(user_choice, ingredients)
+    else:
+        resources["money"] += price
+        print(f"Here is {total_coin_value % price: .2f} in change.")
+        make_coffee(user_choice, ingredients)
+
+
 
 
