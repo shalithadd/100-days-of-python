b61282ad95efa52b22ae43854929e5eb4fc416d8
Shalitha Dehigaspitiyage <shalitha.dehigaspitiyage@gmail.com>
2023-04-02 21:52:47 +0100

    all working and tested

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -3,10 +3,10 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 is_on = True
+coffee_menu = Menu()
+coffee_machine = CoffeeMaker()
+transactions = MoneyMachine()
 while is_on:
-    coffee_menu = Menu()
-    coffee_machine = CoffeeMaker()
-    transactions = MoneyMachine()
     user_choice = input(f"What would you like? ({coffee_menu.get_items()}): ")
 
     if user_choice == "off":
@@ -16,4 +16,7 @@ while is_on:
         transactions.report()
     else:
         user_order = coffee_menu.find_drink(user_choice)
-        coffee_machine.is_resource_sufficient(user_order)
+        is_enough_resources = coffee_machine.is_resource_sufficient(user_order)
+        if is_enough_resources:
+            if transactions.make_payment(user_order.cost):  # pyright:ignore
+                coffee_machine.make_coffee(user_order)
