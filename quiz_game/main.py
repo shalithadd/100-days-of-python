b7b7303ae711d24b924e9841971a255a2a2ab03e
Shalitha Dehigaspitiyage <shalitha.dehigaspitiyage@gmail.com>
2023-04-04 23:05:05 +0100

    basic quiz game completed

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -1,5 +1,6 @@
 from question_model import Questions
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -8,4 +9,8 @@ for question in question_data:
     new_question = Questions(question_text, question_answer)
     question_bank.append(new_question)
 
-print(question_bank)
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+print("You have completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}. ")
