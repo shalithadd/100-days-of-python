9fe2ffe5b0fb3bd15014f4954f685e19a8a44098
Shalitha Dehigaspitiyage <shalitha.dehigaspitiyage@gmail.com>
2023-04-07 16:41:01 +0100

    random walk completed

---
+++ turtle_moudle/random_walk.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+import random
+
+tim = Turtle()
+screen = Screen()
+screen.colormode(255)
+
+
+def walk_random(walk_distance, turn_angle):
+    distance = walk_distance
+    angle = turn_angle
+    directions = random.choice([tim.forward, tim.backward])
+    turns = random.choice([tim.right, tim.left])
+    walk = directions(distance), turns(angle)
+    return walk
+
+
+def random_pencolour():
+    new_colour = []
+    for i in range(3):
+        new_colour.append(random.randint(1, 255))
+    return tuple(new_colour)
+
+
+tim.hideturtle()
+tim.pensize(12)
+tim.speed(8)
+while True:
+    tim.pencolor(random_pencolour())
+    walk_random(30, 90)
+screen.exitonclick()
