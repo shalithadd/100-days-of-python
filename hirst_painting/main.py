fb770256a154deaf0bdbf755828aaec3a9800009
Shalitha Dehigaspitiyage <shalitha.dehigaspitiyage@gmail.com>
2023-04-09 13:32:23 +0100

    hirst_painting fully working

--- hirst_painting/main.py
+++ hirst_painting/main.py
@@ -4,22 +4,26 @@ import random
 
 screen = Screen()
 screen.colormode(255)
-
 tim = Turtle()
 tim.hideturtle()
 tim.speed('fastest')
 tim.penup()
 
-colour = (random.choice(colour_list))
+# Set the starting position
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+numbers_of_dots = 100
 
-
-def fill_circle(random_colour):
-    tim.color(random_colour, random_colour)
-    tim.begin_fill()
-    tim.circle(20)
-    tim.end_fill()
-
-
-fill_circle(colour)
+for dot_count in range(1, numbers_of_dots + 1):
+    tim.dot(20, random.choice(colour_list))
+    tim.forward(50)
+    # Set start position after drawing 10 dots
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
 
 screen.exitonclick()
