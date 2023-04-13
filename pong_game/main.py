6a15f750d253daca1d7b5e81740bdd7560807540
Shalitha Dehigaspitiyage <shalitha.dehigaspitiyage@gmail.com>
2023-04-13 10:27:22 +0100

    pong basic ball bounce done

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 import time
 
 screen = Screen()
@@ -10,7 +11,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
-
+ball = Ball()
 
 screen.listen()
 screen.onkey(key='w', fun=l_paddle.go_up)
@@ -22,5 +23,11 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(.1)
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
 
 screen.exitonclick()
