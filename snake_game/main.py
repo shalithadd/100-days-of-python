b05c80cb61d3bc75ceccbb8bef3ed95fa927f6c4
Shalitha Dehigaspitiyage <shalitha.dehigaspitiyage@gmail.com>
2023-04-16 21:15:37 +0100

    add highscore to snake game

--- snake_game/main.py
+++ snake_game/main.py
@@ -38,13 +38,13 @@ while game_is_on:
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or \
             snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset_score()
+        snake.reset_snake()
 
     # Detect collision with the tale
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 15:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset_score()
+            snake.reset_snake()
 
 screen.exitonclick()
