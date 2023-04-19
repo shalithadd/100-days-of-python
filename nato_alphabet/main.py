6461eaf2bc6f2a1d133d8af053fef32d4eeb4e7f
Shalitha Dehigaspitiyage <shalitha.dehigaspitiyage@gmail.com>
2023-04-19 22:29:33 +0100

    nato alphabet code refined and completed

--- nato_alphabet/main.py
+++ nato_alphabet/main.py
@@ -2,10 +2,9 @@ import pandas
 
 # Create a dict form 'nato_phonetic_alphabet.csv'
 df = pandas.read_csv('nato_phonetic_alphabet.csv')
-nato_alphabet = {row.letter: row.code for letter, row in df.iterrows()}
+nato_alphabet = {row.letter: row.code for (index, row) in df.iterrows()}
 
 # Create a list of the phonetic code words from a word that the user inputs.
-word = list(input('Enter a word: ').upper())
-result = [nato_alphabet[char] for char in word if char in
-          nato_alphabet.keys()]
+word = input('Enter a word: ').upper()
+result = [nato_alphabet[letter] for letter in word]
 print(result)
