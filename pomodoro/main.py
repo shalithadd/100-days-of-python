54916fcd060e45ff540da5b36bd07e6b50afdbdf
Shalitha Dehigaspitiyage <shalitha.dehigaspitiyage@gmail.com>
2023-04-25 23:03:02 +0100

    pomodoro basic ui complete

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -19,11 +19,27 @@ LONG_BREAK_MIN = 20
 root = tk.Tk()
 root.title('Pomodoro')
 root.config(padx=100, pady=50, bg=YELLOW)
-
+# Timer label
+lbl_timer = tk.Label(text='Timer', font=(FONT_NAME, 30, 'bold'), fg=GREEN, bg=YELLOW)
+lbl_timer.grid(pady=10, row=0, column=1)
+# Tomato image
 tomato_img = tk.PhotoImage(file='tomato.png')
 canvas = tk.Canvas(width=200, height=224, bg=YELLOW)
 canvas.create_image(102, 112, image=tomato_img)
+# Timer on top of tomato image
 canvas.create_text(102, 130, text='00:00', fill='white', font=(FONT_NAME, 28, 'bold'))
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+# Start button
+btn_start = tk.Button(text='Start')
+btn_start.grid(row=2, column=0)
+
+# Reset button
+btn_reset = tk.Button(text='Reset')
+btn_reset.grid(row=2, column=2)
+
+# Label to keep track of the number of laps
+lbl_check_marks = tk.Label(text='✔', font=(FONT_NAME, 12, 'normal'), bg=YELLOW, fg=GREEN)
+lbl_check_marks.grid(row=3, column=1)
 
 root.mainloop()
