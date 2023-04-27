4cfe8699bfc59b8fd4f8ea4e6696b3d40b79187f
Shalitha Dehigaspitiyage <shalitha.dehigaspitiyage@gmail.com>
2023-04-27 22:26:53 +0100

    pomodoro app complete

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -9,14 +9,42 @@ YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+LONG_BREAK_MIN = 25
+reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+# noinspection PyTypeChecker
+def reset_timer():
+    try:
+        root.after_cancel(timer)
+        canvas.itemconfig(timer_text, text='00:00')
+        lbl_title.config(text='Timer', fg=GREEN)
+        lbl_check_marks.config(text='')
+        global reps
+        reps = 0
+    except ValueError:
+        print('Timer has not started.')
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(WORK_MIN)
+    global reps
+    reps += 1
+    work_secs = WORK_MIN * 60
+    short_break_secs = SHORT_BREAK_MIN * 60
+    long_break_secs = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_secs)
+        lbl_title.config(text='Long Break', fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_secs)
+        lbl_title.config(text='Break', fg=PINK)
+    else:
+        count_down(work_secs)
+        lbl_title.config(text='Work', fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -29,7 +57,15 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f'{count_mins}:{count_secs}')
     if count > 0:
-        root.after(1000, count_down, count - 1)
+        global timer
+        timer = root.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        check_marks = ''
+        work_sessions_completed = math.floor(reps / 2)
+        for _ in range(work_sessions_completed):
+            check_marks += '✔'
+        lbl_check_marks.config(text=check_marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -38,8 +74,8 @@ root.title('Pomodoro')
 root.config(padx=100, pady=50, bg=YELLOW)
 
 # Timer label
-lbl_timer = tk.Label(text='Timer', font=(FONT_NAME, 30, 'bold'), fg=GREEN, bg=YELLOW)
-lbl_timer.grid(pady=10, row=0, column=1)
+lbl_title = tk.Label(text='Timer', font=(FONT_NAME, 30, 'bold'), fg=GREEN, bg=YELLOW)
+lbl_title.grid(pady=10, row=0, column=1)
 
 # Tomato image
 tomato_img = tk.PhotoImage(file='tomato.png')
@@ -55,11 +91,11 @@ btn_start = tk.Button(text='Start', command=start_timer)
 btn_start.grid(row=2, column=0)
 
 # Reset button
-btn_reset = tk.Button(text='Reset')
+btn_reset = tk.Button(text='Reset', command=reset_timer)
 btn_reset.grid(row=2, column=2)
 
 # Label to keep track of the number of laps
-lbl_check_marks = tk.Label(text='✔', font=(FONT_NAME, 12, 'normal'), bg=YELLOW, fg=GREEN)
+lbl_check_marks = tk.Label(font=(FONT_NAME, 12, 'normal'), bg=YELLOW, fg=GREEN)
 lbl_check_marks.grid(row=3, column=1)
 
 root.mainloop()
