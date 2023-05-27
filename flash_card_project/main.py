b0d0d791ab3b46379f0e317f62fd755ee7614d8f
shalitha.dehigaspitiyage@gmail.com <Dinubaba@60127>
2023-05-27 21:15:33 +0100

    flash card flip function implemented

--- flash_card_project/main.py
+++ flash_card_project/main.py
@@ -1,6 +1,6 @@
+import random
 import tkinter as tk
 import pandas
-import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Arial"
@@ -8,27 +8,39 @@ FONT_NAME = "Arial"
 # Access words in data.csv
 data = pandas.read_csv('data/french_words.csv')
 to_learn = data.to_dict(orient='records')
+current_card = {}
+
+
+def next_card():
+    global current_card, flip_timer
+    root.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text='French', fill='black')
+    canvas.itemconfig(card_word, text=current_card["French"], fill='black')
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = root.after(3000, flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text='English', fill='white')
+    canvas.itemconfig(card_word, text=current_card["English"], fill='white')
+    canvas.itemconfig(card_background, image=card_back_img)
+
 
 root = tk.Tk()
 root.title('Flash Cards')
 root.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
-
-
-def next_card():
-    current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text='French')
-    canvas.itemconfig(card_word, text=current_card["French"])
-
+flip_timer = root.after(3000, flip_card)
 
 # Crate images of front and back of the flash card
-card_front = tk.PhotoImage(file='images/card_front.png')
-card_back = tk.PhotoImage(file='images/card_back.png')
+card_front_img = tk.PhotoImage(file='images/card_front.png')
+card_back_img = tk.PhotoImage(file='images/card_back.png')
 canvas = tk.Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
-canvas.create_image(400, 263, image=card_front)
+card_background = canvas.create_image(400, 263, image=card_front_img)
 
 # Place 'Title' and 'Word' on top of front or back of the card
 card_title = canvas.create_text(400, 150, text='Title', font=(FONT_NAME, 35, 'italic'))
-card_word = canvas.create_text(400, 300, text='Word', font=(FONT_NAME, 50, 'bold'))
+card_word = canvas.create_text(400, 300, text='Word', font=(FONT_NAME, 70, 'bold'))
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Place buttons
