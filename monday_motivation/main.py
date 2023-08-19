7add90d8c9565feacfe74b27d536115dd932477b
shalitha.dehigaspitiyage@gmail.com <shalitha.dehigaspitiyage@gmail.com>
2023-08-20 00:00:23 +0100

    mini project: monday motivation complete

---
+++ monday_motivation/main.py
@@ -0,0 +1,24 @@
+import datetime as dt
+import random
+import smtplib
+
+
+my_email = 'addinusha@gmail.com'
+recipient_email = 'shalithadp@gmail.com'
+my_password = 'wmfeywuitgyjmnen'
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 5:
+    with open('quotes.txt', 'r') as data:
+        list_of_quotes = data.readlines()
+        quote = random.choice(list_of_quotes)
+
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=recipient_email,
+            msg=f'Subject: Quote of the day!\n\n{quote}'
+        )
